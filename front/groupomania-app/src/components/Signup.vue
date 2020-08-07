8d4f7930b30a543052df1f8fc5730954a16da6cd
Emile Bl <bailay.emile@live.fr>
2020-08-07 15:19:08 +0200

    basic vue

---
+++ front/groupomania-app/src/components/Signup.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <form action="" @submit.prevent="register" class="form-example">
+            <div class="form-example">
+                <label for="firstName">Enter your first name: </label>
+                <input type="text" v-model="firstName" name="firstName" id="firstName" required>
+            </div>
+            <div class="form-example">
+                <label for="lastName">Enter your last name: </label>
+                <input type="text" v-model="lastName" name="lastName" id="lastName" required>
+            </div>
+            <div class="form-example">
+                <label for="email">Enter your email: </label>
+                <input type="email" v-model="email" name="email" id="email" required>
+            </div>
+            <div class="form-example">
+                <label for="password">Enter your password: </label>
+                <input type="password" v-model="password" name="password" id="password" required>
+            </div>
+            <div class="form-example">
+                <label for="bio">Enter your bio: </label>
+                <input type="text" v-model="bio" name="bio" id="bio">
+            </div>
+            <div class="form-example">
+                <label for="profilePic">Join your profile picture: </label>
+                <input type="file" @change="uploadImage" name="profilePic" id="profilePic">
+            </div>
+            <div class="form-example">
+                <input type="submit" value="S'inscrire !">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Signup',
+    data () {
+        return {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            bio: '',
+            inputFile: null
+        }
+    },
+    methods:{
+        register() {
+            let newUser = {
+                firstName: this.firstName,
+                lastName: this.lastName,
+                email: this.email,
+                password: this.password,
+                bio: this.bio,
+                inputFile: this.inputFile
+            }
+            console.log(newUser)
+        },
+        uploadImage(e){
+            const image = e.target.files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = e =>{
+                this.inputFile = e.target.result;
+                console.log(this.inputFile);
+            };
+        }
+    }
+    
+}
+</script>
+
+<style>
+form.form-example {
+    display: table;
+}
+
+div.form-example {
+    display: table-row;
+}
+
+label, input {
+    display: table-cell;
+    margin-bottom: 10px;
+}
+
+label {
+    padding-right: 10px;
+}
+</style>
