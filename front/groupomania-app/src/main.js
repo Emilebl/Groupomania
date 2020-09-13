55859909578da491bd390b42216cc5cadfc261f1
Emile Bl <bailay.emile@live.fr>
2020-09-13 18:13:33 +0200

    comments added on main.js

--- front/groupomania-app/src/main.js
+++ front/groupomania-app/src/main.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
+
+// Import Fort Awesome icons library
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faSignOutAlt, faUserEdit, faComment, faComments, faPlusCircle, faSignInAlt, faUserPlus, faMinusCircle, faTrash, faTrashAlt, faPenSquare, faEdit, faThumbsUp, faThumbsDown} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Import vue router
 import VueRouter from 'vue-router';
+
+// Import components
 import Wall from './components/Wall.vue';
 import Signup from './components/Signup.vue';
 import Login from './components/Login.vue';
 import MyProfile from './components/MyProfile.vue';
 import SinglePost from './components/SinglePost.vue'
 
+// Add chosen icons to the library
 library.add(faUser, faSignOutAlt, faUserEdit, faComment, faComments, faPlusCircle, faSignInAlt, faUserPlus, faMinusCircle, faTrash, faTrashAlt, faPenSquare, faEdit, faThumbsUp, faThumbsDown )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
@@ -18,6 +25,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 
+// Define routes for each component
 const routes = [
   {path: '/', component: Wall},
   {path: '/signup', component: Signup},
